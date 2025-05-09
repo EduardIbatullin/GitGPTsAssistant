398a0b7ea545b9e690e402a64809ad9399a1c831
Eduard Ibatullin <168522996+EduardIbatullin@users.noreply.github.com>
2025-05-09 10:40:09 +0500

    feat: добавлен эндпоинт GET /repos/{repo}/commits для получения истории коммитов

--- app/api/routers/gitinfo_router.py
+++ app/api/routers/gitinfo_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from app.domain.services.gitinfo_service import GitInfoService
 from app.api.dependencies import get_github_client
 
@@ -17,5 +17,23 @@ async def get_branches(repo: str, service: GitInfoService = Depends(get_github_c
     """
     try:
         return await service.list_branches(repo)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/repos/{repo}/commits", tags=["Git Metadata"])
+async def get_commits(repo: str, path: str = Query(None), service: GitInfoService = Depends(get_github_client)):
+    """
+    Получить список коммитов для репозитория или конкретного пути (файла/папки).
+
+    Args:
+        repo (str): Название репозитория GitHub.
+        path (str, optional): Путь внутри репозитория (например, src/main.py)
+
+    Returns:
+        list: Список коммитов (SHA, автор, сообщение, дата и т.д.).
+    """
+    try:
+        return await service.list_commits(repo, path)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
