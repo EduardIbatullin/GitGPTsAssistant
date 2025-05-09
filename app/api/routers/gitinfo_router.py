53ed7a36e7fe77eefac3369cbeb7e92379b779d1
Eduard Ibatullin <168522996+EduardIbatullin@users.noreply.github.com>
2025-05-09 22:40:56 +0500

    Добавлены response_model в gitinfo_router.py с использованием моделей Pydantic

--- app/api/routers/gitinfo_router.py
+++ app/api/routers/gitinfo_router.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
+from typing import List
+
 from app.domain.services.gitinfo_service import GitInfoService
 from app.api.dependencies import get_github_client
+from app.domain.models import BranchResponse, CommitResponse, PullRequestResponse, IssueResponse
 
 router = APIRouter()
 
-@router.get("/repos/{repo}/branches", tags=["Git Metadata"])
+@router.get("/repos/{repo}/branches", response_model=List[BranchResponse], tags=["Git Metadata"])
 async def get_branches(repo: str, service: GitInfoService = Depends(get_github_client)):
     try:
         return await service.list_branches(repo)
@@ -12,7 +15,7 @@ async def get_branches(repo: str, service: GitInfoService = Depends(get_github_c
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/repos/{repo}/commits", tags=["Git Metadata"])
+@router.get("/repos/{repo}/commits", response_model=List[CommitResponse], tags=["Git Metadata"])
 async def get_commits(repo: str, path: str = Query(None), service: GitInfoService = Depends(get_github_client)):
     try:
         return await service.list_commits(repo, path)
@@ -20,7 +23,7 @@ async def get_commits(repo: str, path: str = Query(None), service: GitInfoServic
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/repos/{repo}/pulls", tags=["Git Metadata"])
+@router.get("/repos/{repo}/pulls", response_model=List[PullRequestResponse], tags=["Git Metadata"])
 async def get_pull_requests(repo: str, service: GitInfoService = Depends(get_github_client)):
     try:
         return await service.list_pull_requests(repo)
@@ -28,7 +31,7 @@ async def get_pull_requests(repo: str, service: GitInfoService = Depends(get_git
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/repos/{repo}/issues", tags=["Git Metadata"])
+@router.get("/repos/{repo}/issues", response_model=List[IssueResponse], tags=["Git Metadata"])
 async def get_issues(repo: str, service: GitInfoService = Depends(get_github_client)):
     try:
         return await service.list_issues(repo)
